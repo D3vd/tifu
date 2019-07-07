c249d507521c9b5599422a01d587aeddde32e847
Dev <dev.91096@gmail.com>
2019-07-07 07:16:54 +0530

    Added Font buttons

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -8,7 +8,7 @@ export class PostView extends Component {
       <div>
         <header className={styles.headerContainer}>
           <Row>
-            <Col span={4}>
+            <Col span={2}>
               <img
                 onClick={this.props.changeView}
                 className={styles.backBtn}
@@ -16,6 +16,38 @@ export class PostView extends Component {
                 alt=""
               />
             </Col>
+            <div className={styles.mobileFont}>
+              <Col span={2} style={{ float: 'right' }} pull={2}>
+                <img
+                  className={styles.fontSizeBtn}
+                  src={require('./images/decrease.png')}
+                  alt=""
+                />
+              </Col>
+              <Col span={2} style={{ float: 'right' }} pull={6}>
+                <img
+                  className={styles.fontSizeBtn}
+                  src={require('./images/increase.png')}
+                  alt=""
+                />
+              </Col>
+            </div>
+            <div className={styles.desktopFont}>
+              <Col span={2} style={{ float: 'right' }}>
+                <img
+                  className={styles.fontSizeBtn}
+                  src={require('./images/decrease.png')}
+                  alt=""
+                />
+              </Col>
+              <Col span={2} style={{ float: 'right' }}>
+                <img
+                  className={styles.fontSizeBtn}
+                  src={require('./images/increase.png')}
+                  alt=""
+                />
+              </Col>
+            </div>
           </Row>
         </header>
       </div>
